7c630390ff171a51b2fdc0f238de9f7cb9fcfd5f
Nitish <ntaleka@ncsu.edu>
2022-09-12 22:07:06 -0400

    secret

--- app.py
+++ app.py
@@ -83,10 +83,10 @@ def adminpage():
 
 if __name__ == '__main__':
     # app.run(port=5002, debug=True)
-    app.debug = True
     app.secret_key = 'NitishSecret'
-    app.config["SESSION_PERMANENT"] = False
-    app.config["SESSION_TYPE"] = "filesystem"
+    app.debug = True
+    # app.config["SESSION_PERMANENT"] = False
+    # app.config["SESSION_TYPE"] = "filesystem"
     # Session(app)
     # Serve the app with gevent
     http_server = WSGIServer(('0.0.0.0',5000),app)
